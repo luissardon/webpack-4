27eeaad2b1d0f8d70ee418ce98f75ac446f36249
Luis Sardon <lmsardon@gmail.com>
2018-03-16 19:39:54 -0500

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import _ from 'lodash';
 
+import './style.css';
+import Picture from './picture.png';
+import Data from './data.xml';
+
 function component() {
   var element = document.createElement( 'div' );
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add( 'hello' );
+
+  // Add the image to our existing div.
+  var myPicture = new Image();
+  myPicture.src = Picture;
+
+  element.appendChild( myPicture );
+
+  console.log( Data );
 
   return element;
 }
