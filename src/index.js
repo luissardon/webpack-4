0ae488023fde7c49c221dbef9c6a09b83a7f7872
Luis Sardon <lmsardon@gmail.com>
2018-03-16 20:10:02 -0500

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import _ from 'lodash';
-
-import './style.css';
-import Picture from './picture.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement( 'div' );
+  var btn = document.createElement( 'button' );
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add( 'hello' );
 
-  // Add the image to our existing div.
-  var myPicture = new Image();
-  myPicture.src = Picture;
-
-  element.appendChild( myPicture );
-
-  console.log( Data );
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  
+  element.appendChild(btn);
 
   return element;
 }
